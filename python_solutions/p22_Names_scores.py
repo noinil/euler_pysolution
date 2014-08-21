a629d48042a7d0a7db3fcca4fcb89b956f459c64
Cheng Tan <noinil@gmail.com>
2014-08-21 23:04:56 +0900

    problem 22 solved

---
+++ python_solutions/p22_Names_scores.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+def alpha_value(s):
+    return sum([ord(i) - ord('A') + 1 for i in s])
+
+
+with open('../data/p22_names.txt', 'r') as f:
+    str = f.readlines()[0]
+    quoted_names = str.split(sep = ',')
+    names = [i[1:-1] for i in quoted_names]
+
+names.sort()
+print(sum([ (names.index(s) + 1) * alpha_value(s) for s in names]))
