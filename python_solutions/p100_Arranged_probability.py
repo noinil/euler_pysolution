fa93ee9985a8925e5de5a3c941cac14b2e5ce275
Cheng Tan <noinil@gmail.com>
2014-09-25 00:07:57 +0900

    all the first 100 problems solved

--- python_solutions/p100_Arranged_probability.py
+++ python_solutions/p100_Arranged_probability.py
@@ -1,35 +1,22 @@
 #!/usr/bin/env python
 
-import sympy as sp
-
 def main(N):
-    n = 1
+    u, v = N**0.5, 1
     while True:
-        n += 1
-        # print(n)
-        t = 0.5 * (1 + (1 + 2 * n * n - 2 * n) ** 0.5)
-        # if t.is_integer():
-        #     m = round(t)
-            # fcts0 = sp.factorint(n * (n - 1))
-            # fcts0[2] = fcts0[2] - 1
-            # for i in range(-2, 2, 1):
-            #     v = m + i
-                # fcts1 = sp.factorint(v * (v - 1))
-                # if fcts0 == fcts1:
-                #     print("aha!", n, v)
-                #     return 0
-                # if 2 * v * (v - 1) == n * (n - 1):
-                #     print("aha!", n, v)
-                #     return 0
-        m = round(t)
-        for i in range(-2, 3, 1):
-            v = m + i
-            if 2 * v * (v - 1) == n * (n - 1):
-                print("aha!", n, v)
-
-        if n > N:
-            return 0
-
+        u, v = u + 1, 0
+        t1, t2 = round((2 * u * u + 1)**0.5), round((2 * u * u - 1)**0.5)
+        if t1 * t1 == 2 * u * u + 1:
+            v = t1 - u
+        if t2 * t2 == 2 * u * u - 1:
+            v = t2 - u
+        if v != 0:
+            print("m =", u**2 - v**2 + 1, "n =", (u**2+v**2+1)//2)
+            if u**2 - v**2 > N:
+                return 0
 
 if __name__ == '__main__':
-    main(10**5)
+    import time
+    tb = time.time()
+    main(10**12)
+    te = time.time()
+    print("time:", te-tb)
